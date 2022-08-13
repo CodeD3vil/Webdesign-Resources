eeda50088ce092abd0a53099b5d1a03908b6869d
CodeD3vil <tomaskrajcovic@gmail.com>
2022-08-13 11:43:10 -0300

    updated search page

--- public/js/search.js
+++ public/js/search.js
@@ -1,10 +1,20 @@
+const list = document.getElementById("search__list");
+
+document.addEventListener("DOMContentLoaded", function () {
+    list.style.display = "none";
+});
+
 const searchLinks = () => {
     let input = document.getElementById("search_bar").value.toLowerCase();
     let match = document.getElementsByClassName("match");
 
     for (i = 0; i < match.length; i++) {
-        match[i].innerHTML.toLowerCase().includes(input)
-            ? (match[i].style.display = "list-item")
-            : (match[i].style.display = "none");
+        if (input === "") list.style.display = "none";
+        if (match[i].innerHTML.toLowerCase().includes(input) && input !== "") {
+            list.style.display = "block";
+            match[i].style.display = "list-item";
+        } else {
+            match[i].style.display = "none";
+        }
     }
 };
