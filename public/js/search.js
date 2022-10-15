7061884de17d9740aa6c5a16adfccac960c53da1
CodeD3vil <tomaskrajcovic@gmail.com>
2022-10-15 09:58:54 -0300

    started links description

--- public/js/search.js
+++ public/js/search.js
@@ -10,11 +10,12 @@ const searchLinks = () => {
     let match = document.getElementsByClassName("search__result");
 
     for (i = 0; i < match.length; i++) {
+        let matches = `${match[i].getAttribute("data-category").split(",").join(" ")} ${match[i]
+            .getAttribute("data-name")
+            .toLowerCase()}`;
+        match[i].setAttribute("data-search", matches);
         if (input === "") list.style.display = "none";
-        if (
-            (match[i].getAttribute("data-match").includes(input) && input !== "") ||
-            (match[i].getAttribute("data-category").toLowerCase().includes(input) && input !== "")
-        ) {
+        if (match[i].getAttribute("data-search").toLowerCase().includes(input) && input !== "") {
             list.style.display = "block";
             match[i].style.display = "flex";
         } else {
