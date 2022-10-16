ed8ee71a5f22fba7401eb983aa3159283b40d994
CodeD3vil <tomaskrajcovic@gmail.com>
2022-10-16 11:47:37 -0300

    number of searches number return

--- public/js/search.js
+++ public/js/search.js
@@ -1,9 +1,13 @@
 const list = document.getElementById("search__list");
-const searchResults = document.getElementsByClassName("match-link");
 
 document.addEventListener("DOMContentLoaded", function () {
     list.style.display = "none";
 });
+
+const numberOfSearchResults = () => {
+    let searchResults = document.querySelectorAll(".visible");
+    console.log(searchResults.length);
+};
 
 const searchLinks = () => {
     let input = document.getElementById("search_bar").value.toLowerCase();
@@ -14,12 +18,17 @@ const searchLinks = () => {
             .getAttribute("data-name")
             .toLowerCase()}`;
         match[i].setAttribute("data-search", matches);
+
         if (input === "") list.style.display = "none";
         if (match[i].getAttribute("data-search").toLowerCase().includes(input) && input !== "") {
             list.style.display = "block";
             match[i].style.display = "flex";
+            match[i].classList.add("visible");
         } else {
             match[i].style.display = "none";
+            match[i].classList.remove("visible");
         }
     }
+
+    numberOfSearchResults();
 };
